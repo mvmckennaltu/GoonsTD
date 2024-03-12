8b22f5ebacdd69b001ca7e3815d0c63476f718da
mvmckennaltu <cmckenna@ltu.edu>
2024-03-11 21:20:45 -0400

    Final Unfinished Build
    I'm giving up. Gonna put my all into the next project.

--- Assets/Scripts/MainTower.cs
+++ Assets/Scripts/MainTower.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class MainTower : MonoBehaviour
 {
+    public int mainHP;
+    public int mainMaxHP;
+
     // Start is called before the first frame update
     void Start()
     {
